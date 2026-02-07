9e460f3e50b88a54382a3e904326f634c8657212
Jeevan Chandimal <your-github-email@example.com>
2026-02-07 18:36:15 +0530

    fix global styles import

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import './style.css'
+import '../style.css'
 
 import { GlobalProvider } from '../global-context'
 import { NextIntlProvider } from 'next-intl'
+
 export default function MyApp({ Component, pageProps }) {
   return (
     <NextIntlProvider messages={pageProps?.messages}>
